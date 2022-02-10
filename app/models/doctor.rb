9c836da12b5c98f3d37158b57393d250d09d7401
WilfriedPaillot <paillotwilfried@gmail.com>
2022-02-10 18:47:32 +0100

    Added city migration & model + speciality

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,9 @@
 class Doctor < ApplicationRecord
   # A plusieurs rdv
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
+  has_many :doctor_specialities, dependent: :destroy
   # A plusieurs patients, à travers les rdv
   has_many :patients, through: :appointments
+  has_many :specialities, through: :doctor_specialities
+  belongs_to :city
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ApplicationRecord
   # A plusieurs rdv
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   # A plusieurs médecins, à travers appointments
   has_many :doctors, through: :appointments
+  belongs_to: city
 end
